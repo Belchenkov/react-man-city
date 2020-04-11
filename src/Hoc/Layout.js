b06386af429faa66248982b0f24cb8500021169e
Aleksey Belchenkov <u608110@gmail.com>
2020-04-11 17:02:38 +0300

    Add Footer

--- src/Hoc/Layout.js
+++ src/Hoc/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from "../components/header_footer/Header";
+import Footer from "../components/header_footer/Footer";
 
 const Layout = ({ children }) => {
     return (
         <div>
             <Header />
             { children }
+            <Footer />
         </div>
     );
 };
