4f88bc0f9eeaa283b6b7b627175f1adb3f2fe7b7
Aleksey Belchenkov <u608110@gmail.com>
2020-04-11 16:58:56 +0300

    Add Header Icon

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -4,6 +4,8 @@ import { Toolbar } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
+import CityLogo from "../ui/icons";
+
 class Header extends Component {
     render() {
         return (
@@ -17,18 +19,25 @@ class Header extends Component {
                }}
            >
                <Toolbar
-                   styles={{ display: 'flex' }}
+                   style={{ display: 'flex' }}
                >
-                   <div styles={{ flexGrow: 1 }}>
+                   <div style={{ flexGrow: 1 }}>
                         <div className="header_logo">
-                            <Link to="/the_team">
-                                <Button color="inherit">The team</Button>
-                            </Link>
-                            <Link to="/the_matches">
-                                <Button color="inherit">The team</Button>
-                            </Link>
+                            <CityLogo
+                                link={true}
+                                linkTo="/"
+                                width="70px"
+                                height="70px"
+                            />
                         </div>
                    </div>
+
+                   <Link to="/the_team">
+                       <Button color="inherit">The team</Button>
+                   </Link>
+                   <Link to="/the_matches">
+                       <Button color="inherit">The team</Button>
+                   </Link>
                </Toolbar>
            </AppBar>
         );
