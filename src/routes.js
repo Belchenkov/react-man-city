c4cb650b5e078bb4e04763a62c46af15c903a8ab
Aleksey Belchenkov <u608110@gmail.com>
2020-05-02 10:45:04 +0300

    Auth Creating a Dashboard

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Layout from "./hoc/Layout";
 import Home from "./components/home";
 import SignIn from "./components/sign_in";
+import Dashboard from "./components/admin/Dashboard";
 
 const Routes = () => {
     return (
@@ -11,6 +12,7 @@ const Routes = () => {
             <Switch>
                 <Route exact path="/" component={ Home } />
                 <Route exact path="/sign_in" component={ SignIn } />
+                <Route exact path="/dashboard" component={ Dashboard } />
             </Switch>
         </Layout>
     );
