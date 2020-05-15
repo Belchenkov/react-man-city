92e627c0ccf681ce19affc93f34cbe0b6c78dae6
Aleksey Belchenkov <u608110@gmail.com>
2020-05-15 10:57:02 +0300

    FileUploader Input

--- src/components/ui/file_uploader.js
+++ src/components/ui/file_uploader.js
@@ -11,6 +11,34 @@ class FileUploader extends Component {
         fileURL: ''
     }
 
+    handleUploadStart = () => {
+        this.setState({
+            isUploading: true
+        });
+    }
+
+    handleUploadError = () => {
+        this.setState({
+            isUploading: false
+        });
+    }
+
+    handleUploadSuccess = fileName => {
+        this.setState({
+            name: fileName,
+            isUploading: false
+        });
+
+        firebase.storage().ref(this.props.dir)
+            .child(fileName)
+            .getDownloadURL()
+            .then(url => {
+                this.setState({
+                    fileURL: url
+                });
+            });
+    }
+
     static getDerivedStateFromProps(props, state) {
         if (props.defaultImg) {
             return state = {
@@ -38,6 +66,37 @@ class FileUploader extends Component {
                                     onUploadError={ this.handleUploadError }
                                     onUploadSuccess={ this.handleUploadSuccess }
                                 />
+                            </div>
+                        )
+                        : null
+                }
+                {
+                    this.state.isUploading
+                        ? (
+                            <div
+                                className="progress"
+                                style={{ textAlign: 'center', margin: '30px 0' }}
+                            >
+                                <CircularProgress
+                                    style={{ color: '#98c6e9' }}
+                                    thickness={7}
+                                />
+                            </div>
+                        )
+                        : null
+                }
+                {
+                    this.state.fileURL
+                        ? (
+                            <div className="image_upload_container">
+                                <img
+                                    src={this.state.fileURL}
+                                    style={{ width: '100%' }}
+                                    alt={this.state.name}
+                                />
+                                <div className="remove" onClick={() => this.uploadAgain()}>
+                                    Remove
+                                </div>
                             </div>
                         )
                         : null
--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
+import 'firebase/storage';
 
 const config = {
     apiKey: "AIzaSyCY0BAHw-urY4y3wQBfNEyfGwLQAeOroKU",
